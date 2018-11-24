254a32fd0aeb1e2b23b7678e568549905b0d9927
Joosep Alviste <joosep.alviste@gmail.com>
2018-11-24 18:27:37 +0200

    Quick fix

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -23,7 +23,7 @@ class ChatBot:
         if self.historybuffer.__sizeof__() == 10:
             self.historybuffer.pop()
         self.knowledge = self.sentiment.analyse_text(self.knowledge, input)
-        if "@bot" or "@Bot" in input:
+        if "@bot" in input or "@Bot" in input:
             return self.reply(input, "location")
         return self.reply(input, "aiml")
 
