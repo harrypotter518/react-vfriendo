781658ba16e707bc8a15d92463e58cc4dc939284
Joosep Alviste <joosep.alviste@gmail.com>
2018-11-24 12:20:57 +0200

    Some initial Chat component

---
+++ frontend/src/components/Chat/Chat.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+import './Chat.css'
+
+class Chat extends Component {
+  state = {
+    messages: [
+      { from: 1, contents: 'Hello!' },
+      { from: 0, contents: 'Hello to you too!' },
+      { from: 1, contents: "What's up?" },
+    ],
+  }
+
+  render() {
+    const { messages } = this.state
+
+    return (
+      <div className="chat__container">
+        <div className="chat__header">
+          <div className="header__container">
+            <div>
+              <div className="header__name">Some Guy</div>
+              <div className="header__status">Active now</div>
+            </div>
+          </div>
+        </div>
+        <div className="chat__content">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`message__container ${
+                message.from === 1
+                  ? 'message__container--mine'
+                  : 'message__container--others'
+              }`}
+            >
+              <div className="message">{message.contents}</div>
+            </div>
+          ))}
+        </div>
+        <form className="input__container">
+          <input
+            className="input__input"
+            type="text"
+            placeholder="Type your message..."
+            name="message"
+          />
+          <button type="submit" className="input__submit">
+            <svg
+              x="0px"
+              y="0px"
+              viewBox="0 0 448.011 448.011"
+              style={{ enableBackground: 'new 0 0 448.011 448.011' }}
+            >
+              <g>
+                <path d="M438.731,209.463l-416-192c-6.624-3.008-14.528-1.216-19.136,4.48c-4.64,5.696-4.8,13.792-0.384,19.648l136.8,182.4    l-136.8,182.4c-4.416,5.856-4.256,13.984,0.352,19.648c3.104,3.872,7.744,5.952,12.448,5.952c2.272,0,4.544-0.48,6.688-1.472    l416-192c5.696-2.624,9.312-8.288,9.312-14.528S444.395,212.087,438.731,209.463z" />
+              </g>
+            </svg>
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Chat
